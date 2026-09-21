225b09e899edd9a62d0c54759865cde4345fd497
gusadi09 <gusadi872@gmail.com>
2026-09-21 13:38:25 +0700

    Initial Commit

---
+++ MessageAndLiveness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MessageAndLiveness
+//
+//  Created by Ewide Dev 5 on 21/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MessageAndLiveness/MessageAndLivenessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageAndLivenessApp.swift
+//  MessageAndLiveness
+//
+//  Created by Ewide Dev 5 on 21/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MessageAndLivenessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
